0401a63b5aa61f80672168248e2fb890f77f19ca
Luka Burian <lukaburian@gmail.com>
2022-12-21 19:16:48 +0100

    end phase 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
+import Blogs from "./pages/Step1/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
